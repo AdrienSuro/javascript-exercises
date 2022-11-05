6fd453a726bd075284952dd323b3a396d879774b
AdrienSuro <adrien.surowiec@protonmail.com>
2022-11-05 18:18:29 +0100

    Add solution to exercise1

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let booksOnly = [];
+    booksOnly.push(books[0].title) ;
+    booksOnly.push(books[1].title) ;
+    return booksOnly ; 
 };
 
 // Do not edit below this line

c22f2c58fd4db95ecc3318e9cf65771a19d3005d
AdrienSuro <adrien.surowiec@protonmail.com>
2022-11-05 17:32:02 +0100

    Add solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+if (x<=0) {
+    return "OOPS";
+}
+else {
+    let array = [1, 1];
+    for (let i=0 ; i<x-2 ; i++) {
+        array.push(array[(array.length)-1] + array[(array.length)-2])
+    }
+return array[x-1];
+}
 };
 
 // Do not edit below this line
